91061a0676ee921ce3a5d90b82295a4048980bcd
pk9rprotein <72692627+pk9rprotein@users.noreply.github.com>
2022-03-04 18:24:02 +0700

    Fix BUGGGGG

--- Pikachu/GameControl/GamePlayChecker.cs
+++ Pikachu/GameControl/GamePlayChecker.cs
@@ -8,14 +8,7 @@ using System.Threading.Tasks;
 
 namespace Pikachu.GameControl
 {
-	/// <summary>
-	///   <para>Đối tượng kiểm tra, xử lý các thao tác của GamePlay.</para>
-	///   <para>
-	///     <strong>CẢNH BÁO</strong>
-	///   </para>
-	///   <para>Đoạn code dưới đây là ma thuật</para>
-	///   <para>Lúc tôi code chỉ có tôi và chúa hiểu giờ thì chỉ có mình chúa mới hiểu</para>
-	/// </summary>
+	/// <summary>Đối tượng kiểm tra, xử lý các thao tác của GamePlay.</summary>
 	internal class GamePlayChecker
 	{
 		readonly DataGamePlay dataGamePlay;
@@ -29,11 +22,6 @@ namespace Pikachu.GameControl
 		}
 
 		/// <summary>Lấy các đường thẳng nối cặp pokemon với nhau.</summary>
-		/// <param name="r1">The r1.</param>
-		/// <param name="c1">The c1.</param>
-		/// <param name="r2">The r2.</param>
-		/// <param name="c2">The c2.</param>
-		/// <returns>Các đường thẳng nối cặp pokemon được chọn lại với nhau.</returns>
 		public List<LineConnect> GetLines(int r1, int c1, int r2, int c2)
 		{
 			List<LineConnect> lines = new();
@@ -58,8 +46,26 @@ namespace Pikachu.GameControl
 				return lines;
 			}
 
-			int row = CheckRectR(r1, c1, r2, c2);
-			if (row != -1)
+			int col, row;
+
+			col = CheckL(r1, c1, r2, c2);
+			if (col != -2)
+			{
+				if (col == c1)
+				{
+					lines.Add(new LineConnect(r1, c1, r2, c1));
+					lines.Add(new LineConnect(r2, c1, r2, c2));
+				}
+				else
+				{
+					lines.Add(new LineConnect(r1, c1, r1, c2));
+					lines.Add(new LineConnect(r1, c2, r2, c2));
+				}
+				return lines;
+			}
+
+			row = Check3R(r1, c1, r2, c2);
+			if (row != -2)
 			{
 				lines.Add(new LineConnect(r1, c1, row, c1));
 				lines.Add(new LineConnect(row, c1, row, c2));
@@ -67,71 +73,16 @@ namespace Pikachu.GameControl
 				return lines;
 			}
 
-			int col = CheckRectC(r1, c1, r2, c2);
-			if (col != -1)
+			col = Check3C(r1, c1, r2, c2);
+			if (col != -2)
 			{
 				lines.Add(new LineConnect(r1, c1, r1, col));
 				lines.Add(new LineConnect(r1, col, r2, col));
 				lines.Add(new LineConnect(r2, col, r2, c2));
-				return lines;
-			}
-
-			col = CheckMoreLineR(r1, c1, r2, c2, 1);
-			if (col != -1)
-			{
-				lines.Add(new LineConnect(r1, c1, r1, col));
-				lines.Add(new LineConnect(r1, col, r2, col));
-				lines.Add(new LineConnect(r2, col, r2, c2));
-				return lines;
-			}
-
-			col = CheckMoreLineR(r1, c1, r2, c2, -1);
-			if (col != -1)
-			{
-				lines.Add(new LineConnect(r1, c1, r1, col));
-				lines.Add(new LineConnect(r1, col, r2, col));
-				lines.Add(new LineConnect(r2, col, r2, c2));
-				return lines;
-			}
-
-			row = CheckMoreLineC(r1, c1, r2, c2, 1);
-			if (row != -1)
-			{
-				lines.Add(new LineConnect(r1, c1, row, c1));
-				lines.Add(new LineConnect(row, c1, row, c2));
-				lines.Add(new LineConnect(row, c2, r2, c2));
-				return lines;
-			}
-
-			row = CheckMoreLineC(r1, c1, r2, c2, -1);
-			if (row != -1)
-			{
-				lines.Add(new LineConnect(r1, c1, row, c1));
-				lines.Add(new LineConnect(row, c1, row, c2));
-				lines.Add(new LineConnect(row, c2, r2, c2));
 				return lines;
 			}
 
 			return lines;
-		}
-
-		/// <summary>Kiểm tra đường thẳng 1 hàng.</summary>
-		/// <param name="c1">The c1.</param>
-		/// <param name="c2">The c2.</param>
-		/// <param name="r">The r.</param>
-		/// <returns>
-		///   <br />
-		/// </returns>
-		private bool CheckLineR(int c1, int c2, int r)
-		{
-			int min = c1, max = c2;
-			if (min > max) { max = c1; min = c2; }
-
-			for (int col = min; col <= max; col++)
-				if (IsBarrier(r, col))
-					return false;
-
-			return true;
 		}
 
 		/// <summary>Kiểm tra toạ độ rào chắn.</summary>
@@ -142,6 +93,18 @@ namespace Pikachu.GameControl
 			return (dataGamePlay.GetValue(row, col) != 0 &&
 				(row != rowE1 || col != colE1) &&
 				(row != rowE2 || col != colE2));
+		}
+
+		private bool CheckLineR(int c1, int c2, int r)
+		{
+			int min = c1, max = c2;
+			if (min > max) { max = c1; min = c2; }
+
+			for (int col = min; col <= max; col++)
+				if (IsBarrier(r, col))
+					return false;
+
+			return true;
 		}
 
 		private bool CheckLineC(int r1, int r2, int c)
@@ -156,106 +119,64 @@ namespace Pikachu.GameControl
 			return true;
 		}
 
-		private int CheckRectR(int r1, int c1, int r2, int c2)
+		private int CheckL(int r1, int c1, int r2, int c2)
 		{
-			int rpRMin = r1, cpRMin = c1, rpRMax = r2, cpRMax = c2;
-			if (rpRMin > rpRMax)
+			if (CheckLineR(c1, c2, r1) && CheckLineC(r1, r2, c2))
 			{
-				cpRMax = c1; cpRMin = c2;
-				rpRMax = r1; rpRMin = r2;
+				return c2;
 			}
 
-			for (int row = rpRMin + 1; row < rpRMax; row++)
+			if (CheckLineC(r1, r2, c1) && CheckLineR(c1, c2, r2))
 			{
-				if (CheckLineC(rpRMin, row, cpRMin) &&
-					CheckLineR(cpRMin, cpRMax, row) &&
-					CheckLineC(row, rpRMax, cpRMax))
-					return row;
+				return c1;
 			}
 
-			return -1;
+			return -2;
 		}
 
-		private int CheckRectC(int r1, int c1, int r2, int c2)
+		private int Check3R(int r1, int c1, int r2, int c2)
 		{
-			int rpCMin = r1, cpCMin = c1, rpCMax = r2, cpCMax = c2;
-			if (cpCMin > cpCMax)
-			{
-				cpCMax = c1; cpCMin = c2;
-				rpCMax = r1; rpCMin = r2;
-			}
+			int rowP = r1, rowS = r1;
 
-			for (int col = cpCMin + 1; col < cpCMax; col++)
+			while (rowP <= GamePlay.totalRows || rowS >= 0)
 			{
-				if (CheckLineR(cpCMin, col, rpCMin) &&
-					CheckLineC(rpCMin, rpCMax, col) &&
-					CheckLineR(col, cpCMax, rpCMax))
-					return col;
-			}
-
-			return -1;
-		}
-
-		private int CheckMoreLineC(int r1, int c1, int r2, int c2, int type)
-		{
-			int rpCMin = r1, cpCMin = c1, rpCMax = r2, cpCMax = c2;
-			if (cpCMin > cpCMax)
-			{
-				cpCMax = c1; cpCMin = c2;
-				rpCMax = r1; rpCMin = r2;
-			}
-
-			int row = cpCMax;
-			int col = cpCMin;
-			if (type == -1)
-			{
-				row = rpCMin;
-				col = cpCMax;
-			}
-			if (CheckLineC(cpCMin, rpCMax, col))
-			{
-				while (!IsBarrier(cpCMin, row)
-						&& !IsBarrier(cpCMax, row))
+				rowP++; 
+				rowS--;
+				if (CheckL(r1, c1, rowP, c2) != -2 &&
+					CheckL(rowP, c1, r2, c2) != -2)
 				{
-					if (CheckLineR(cpCMin, cpCMax, row))
-					{
-						return row;
-					}
-					row += type;
+					return rowP;
+				}
+				if (CheckL(r1, c1, rowS, c2) != -2 &&
+					CheckL(rowS, c1, r2, c2) != -2)
+				{
+					return rowS;
 				}
 			}
-			return -1;
+			return -2;
 		}
 
-		private int CheckMoreLineR(int r1, int c1, int r2, int c2, int type)
+		private int Check3C(int r1, int c1, int r2, int c2)
 		{
-			int rpRMin = r1, cpRMin = c1, rpRMax = r2, cpRMax = c2;
-			if (rpRMin > rpRMax)
+			int colP = c1, colS = c1;
+
+			while (colP <= GamePlay.totalColumns || colS >= 0)
 			{
-				cpRMax = c1; cpRMin = c2;
-				rpRMax = r1; rpRMin = r2;
+				colP++;
+				colS--;
+				if (CheckL(r1, c1, r2, colP) != -2 &&
+					CheckL(r1, colP, r2, c2) != -2)
+				{
+					return colP;
+				}
+				if (CheckL(r1, c1, r2, colS) != -2 &&
+					CheckL(r1, colS, r2, c2) != -2)
+				{
+					return colS;
+				} 
 			}
 
-			int col = cpRMax;
-			int row = rpRMin;
-			if (type == -1)
-			{
-				col = cpRMin;
-				row = rpRMax;
-			}
-			if (CheckLineR(cpRMin, cpRMax, row))
-			{
-				while (!IsBarrier(col, rpRMin)
-						&& !IsBarrier(col, rpRMax))
-				{
-					if (CheckLineC(rpRMin, rpRMax, col))
-					{
-						return col;
-					}
-					col += type;
-				}
-			}
-			return -1;
+			return -2;
 		}
 	}
 }
